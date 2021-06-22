b4b466d8ba21f361097939a52c55f69859909dff
Antony Derham <antonyderham@gmail.com>
2021-06-22 23:17:26 +0100

    refactor: Move to docs/

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,16 +1,16 @@
-import {src, dest, watch} from 'gulp';
+import {parallel, src, dest, watch} from 'gulp';
 import markdown from 'gulp-markdown';
 import headerfooter from 'gulp-headerfooter';
 
 const css = cb => {
     src('css/*.css')
-        .pipe(dest('dist/css'));
+        .pipe(dest('docs/css'));
     cb();
 };
 
 const fonts = cb => {
     src('fonts/*')
-        .pipe(dest('dist/fonts'));
+        .pipe(dest('docs/fonts'));
     cb();
 };
 
@@ -19,12 +19,16 @@ const md = cb => {
         .pipe(markdown())
         .pipe(headerfooter.header('./header.html'))
         .pipe(headerfooter.footer('</body></html>'))
-        .pipe(dest('dist'));
+        .pipe(dest('docs'));
     cb();
 };
 
-export default () => {
+const watchfiles = () => {
     watch('css/*', css);
     watch('fonts/*', fonts);
     watch('*.md', md);
 }
+
+const compile = parallel(css, fonts, md);
+
+export default compile;
